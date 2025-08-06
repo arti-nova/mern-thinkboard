c6ba11746863ffcab942b32d4ae41556fb4f526f
arti-nova <artidwivedi551@gmail.com>
2025-08-06 23:33:22 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from"path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from './config/db.js';
@@ -10,21 +11,31 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB(); 
-// Middleware 
+const __dirname =path.resolve()
+
+// Middleware
+if(process.env.NODE_ENV !== "production") { 
 app.use
 (cors({
    origin: 'http://localhost:5173', 
   })
 );
+};
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimit); // Apply rate limiting middleware
 
-app.use("/api/notes",rateLimit , notesRoutes);
-
+app.use("/api/notes", notesRoutes);
+ 
+if(process.env.NODE_ENV ==="production") {
+   app.use(express.static(path.join(__dirname,"../frontend/dist")));
+   app.get("*", (req,res) => {
+     res.sendFile(path.join(__dirname,"../frontend", "dist","index.html"));
+    });
+};
+connectDB().then (() => { 
 app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
 
-
+});
 });
    
